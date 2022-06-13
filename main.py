174b154a4e4f75b78e94ee04fb4bc5373892008b
hinpile <104378017+hinpile@users.noreply.github.com>
2022-06-13 20:52:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ while len(dec1) < len(msg1) or len(dec1) < len(msg2):
     guessing: int = int(input("guessing: "))
     guess: str = input("guess: ")
     if guessing == 1:
-        res = drag(msg1, msg2, dec1, dec2, guess, key)
+        res: list = drag(msg1, msg2, dec1, dec2, guess, key)
     else:
-        res = drag(msg2, msg1, dec2, dec1, guess, key)
+        res: list = drag(msg2, msg1, dec2, dec1, guess, key)
     dec1 = res[(guessing % 2) ^ 1]
     dec2 = res[guessing % 2]
     key = res[2]
