24275b38f356d122522a0d06c0429089b0e11a2e
hinpile <104378017+hinpile@users.noreply.github.com>
2022-06-13 20:50:34 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import onetimepad
+
+msg1: str = input("1st message: ")
+msg2: str = input("2nd message: ")
+dec1: str = ""
+dec2: str = ""
+key: str = ""
+
+
+def drag(m: str, n: str, d: str, e: str, g: str, k: str) -> list:
+    a: int = len(d)
+    b: int = a + 1
+    d += g + "."
+    x: str = m[a] + m[b]
+    y: str = onetimepad.decrypt(x, g)
+    k += y + "."
+    e += (onetimepad.decrypt(n[a] + n[b], y) if len(n) > a else " ") + "."
+    return [d, e, k]
+
+
+while True:
+    guessing: int = int(input("guessing: "))
+    guess: str = input("guess: ")
+    if guessing == 1:
+        res = drag(msg1, msg2, dec1, dec2, guess, key)
+    else:
+        res = drag(msg2, msg1, dec2, dec1, guess, key)
+    dec1 = res[(guessing % 2) ^ 1]
+    dec2 = res[guessing % 2]
+    key = res[2]
+    print(dec1)
+    print(dec2)
+    if len(dec1) >= len(msg1) and len(dec1) >= len(msg2):
+        break
+
+print()
+print(msg1)
+print(msg2)
+print()
+print(key)
